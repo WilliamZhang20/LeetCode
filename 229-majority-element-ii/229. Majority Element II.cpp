f016b8afb26adbcd2227c72210361c6bba9f42b6
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2025-05-09 18:30:32 -0400

    Runtime: 3 ms (18.44%), Memory: 23.6 MB (5.81%) - Gitcode

--- 229-majority-element-ii/229. Majority Element II.cpp
+++ 229-majority-element-ii/229. Majority Element II.cpp
@@ -1,17 +1,47 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> freq;
+        // Using Bayer-More Majority Voting Algorithm
+        int majority1=0, majority2=0, count1=0, count2 = 0;
         for(int i=0; i<nums.size(); i++) {
-            freq[nums[i]] += 1;
-        }
-        vector<int> result;
-        int n = nums.size();
-        for(auto m = freq.begin(); m != freq.end(); m++) {
-            if(m->second > floor(n/3)) {
-                result.push_back(m->first);
+            if(nums[i] == majority1) {
+                ++count1;
+            }
+            else if(nums[i] == majority2) {
+                ++count2;
+            } 
+            else if(count1 == 0) {
+                majority1 = nums[i];
+                ++count1;
+            }
+            else if(count2 == 0) {
+                majority2 = nums[i];
+                ++count2;
+            } 
+            else {
+                --count1;
+                --count2;
             }
         }
-        return result;
+        count1 = 0;
+        count2 = 0;
+        cout << majority1 << "\n";
+        // second step for verification
+        for(int i=0; i<nums.size(); i++) {
+            if(nums[i] == majority1) {
+                ++count1;
+            }
+            else if(nums[i] == majority2) {
+                ++count2;
+            }
+        }
+        vector<int> res;
+        if(count1 > floor(nums.size()/3)) {
+            res.push_back(majority1);
+        }
+        if(count2 > floor(nums.size()/3)) {
+            res.push_back(majority2);
+        }
+        return res;
     }
 };
