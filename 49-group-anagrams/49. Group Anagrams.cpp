22110ff2ac52b39b62842f7c2afabc02de2ce993
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2025-05-14 20:37:39 -0400

    Runtime: 21 ms (38.00%), Memory: 26.1 MB (37.06%) - Gitcode

---
+++ 49-group-anagrams/49. Group Anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> freq;
+        for(int i=0; i<strs.size(); i++) {
+            string curr = strs[i];
+            std::sort(curr.begin(), curr.end());
+            freq[curr].push_back(strs[i]);
+        }
+        vector<vector<string>> groups;
+        for(auto e : freq) {
+            groups.push_back(e.second);
+        }
+        return groups;
+    }
+};
