8f78b8552e5491ed5c483fdd121df57c3f3e43ea
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2025-05-15 15:55:02 -0400

    Runtime: 0 ms (100.00%), Memory: 17.7 MB (84.69%) - Gitcode

---
+++ 152-maximum-product-subarray/152. Maximum Product Subarray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int maxProduct = INT_MIN;
+        int prod = 1;
+        for(int i=0; i<nums.size(); i++) {
+            prod *= nums[i];
+            maxProduct = max(maxProduct, prod);
+            if(prod == 0) {
+                prod = 1;
+            }
+        }
+        prod = 1;
+        for(int i=nums.size() - 1; i >= 0; i--) {
+            prod *= nums[i];
+            maxProduct = max(maxProduct, prod);
+            if(prod == 0) {
+                prod = 1;
+            }
+        }
+        return maxProduct;
+    }
+};
