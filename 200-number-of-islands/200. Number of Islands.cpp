288082a24c251e2c7c1f1f312842186db035561c
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2025-05-19 12:41:26 -0400

    Runtime: 19 ms (96.10%), Memory: 16.2 MB (88.58%) - Gitcode

--- 200-number-of-islands/200. Number of Islands.cpp
+++ 200-number-of-islands/200. Number of Islands.cpp
@@ -1,36 +1,33 @@
 class Solution {
+    int m;
+    int n;
+    vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
 public:
+    void dfs(int i, int j, vector<vector<char>>& grid) {
+        grid[i][j] = '0';
+        for(vector<int>& dir : dirs) {
+            int nx = i + dir[0];
+            int ny = j + dir[1];
+            if(nx < 0 || nx >= m || ny < 0 || ny >= n) {
+                continue;
+            }
+            if(grid[nx][ny] == '1') {
+                dfs(nx, ny, grid);
+            }
+        }
+    }
     int numIslands(vector<vector<char>>& grid) {
-        int result = 0;
+        int count = 0;
         m = grid.size();
         n = grid[0].size();
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
                 if(grid[i][j] == '1') {
-                    std::cout << "incr\n";
-                    result++;
+                    count += 1;
                     dfs(i, j, grid);
                 }
             }
         }
-        return result;
-    }
-private:
-    int m;
-    int n;
-    std::vector<std::vector<int>> dirs = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
-    void dfs(int i, int j, vector<vector<char>>& grid) {
-        std::cout << i << " " << j << "\n";
-        grid[i][j] = '0';
-        for(vector<int>& v : dirs) {
-            int x = i + v[0];
-            int y = j + v[1];
-            if(x < 0 || x >= m || y < 0 || y >= n) {
-                continue;
-            }
-            if(grid[x][y] == '1') {
-                dfs(x, y, grid);
-            }
-        }
+        return count;
     }
 };
