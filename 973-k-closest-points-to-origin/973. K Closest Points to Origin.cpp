4bc4048fe28599a359bac25646ff6364163c84f8
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2025-06-26 10:31:38 -0400

    Runtime: 359 ms (7.40%), Memory: 68.7 MB (64.22%) - Gitcode

---
+++ 973-k-closest-points-to-origin/973. K Closest Points to Origin.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<double, int>, vector<pair<double, int>>> pq; // min pq
+        for(int i=0; i<points.size(); i++) {
+            double dist = sqrt(pow(points[i][0], 2) + pow(points[i][1], 2));
+            cout << dist << endl;
+            pq.push({dist, i});
+            if(pq.size() > k) {
+                pq.pop();
+            }
+        }
+        vector<vector<int>> result;
+        while (!pq.empty()) {
+            result.push_back(points[pq.top().second]);
+            pq.pop();                    // Remove the top element
+        }
+        return result;
+    }
+};
