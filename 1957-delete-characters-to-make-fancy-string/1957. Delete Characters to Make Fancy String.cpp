64a613772f865e4cc82e1150361cce05797427a7
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2025-07-20 20:32:42 -0400

    Runtime: 20 ms (84.63%), Memory: 44.4 MB (67.47%) - Gitcode

---
+++ 1957-delete-characters-to-make-fancy-string/1957. Delete Characters to Make Fancy String.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        if (s.length() < 3) return s;
+        int count = 1;
+        std::string result;
+        result += s[0];
+        for(int i=1; i<s.length(); i++) {
+            if(s[i] == s[i-1]) {
+                count++;
+                if(count < 3) {
+                    result += s[i];
+                }
+            } else {
+                count = 1;
+                result += s[i];
+            }
+        }
+        return result;
+    }
+};
