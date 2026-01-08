173576e2d631a51c22effd1b16e096a1e7e086c2
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2026-01-08 18:34:36 -0500

    Runtime: 7 ms (96.73%), Memory: 17.7 MB (65.71%) - Gitcode

---
+++ 1458-max-dot-product-of-two-subsequences/1458. Max Dot Product of Two Subsequences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, INT_MIN));
+        int res = INT_MIN;
+        for(int i=n1-1; i>=0; i--) {
+            for(int j=n2-1; j>=0; j--) {
+                int x = nums1[i] * nums2[j], tmp=dp[i&1][j];
+                tmp = max(tmp, x);
+                tmp = max(tmp, x+(i+1 < n1 & j+1 < n2 ? dp[(i+1)&1][j+1] : 0));
+                tmp = max(tmp, dp[i&1][j+1]);
+                dp[i&1][j] = max(tmp, dp[(i+1)&1][j]);
+                res = max(res, dp[i&1][j]);
+            }
+        }
+        return res;
+    }
+};
