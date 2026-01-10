947e7342022e4b66a9ff52cb3300574195874f59
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2026-01-10 11:01:11 -0500

    Runtime: 4 ms (14.59%), Memory: 70.7 MB (62.88%) - Gitcode

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574. Shortest Subarray to be Removed to Make Array Sorted.cpp
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574. Shortest Subarray to be Removed to Make Array Sorted.cpp
@@ -1,34 +1,18 @@
 class Solution {
 public:
-    int helperBinarySearch(vector<int>& arr, int left, int right, int target) {
-        while(left < right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] >= target) 
-                right = mid;
-            else
-                left = mid + 1;
-        }
-        return left;
-    }
     int findLengthOfShortestSubarray(vector<int>& arr) {
         int n = arr.size();
-        int left = 0;
-        int right = n-1;
-        while(left + 1 < n && arr[left] <= arr[left+1]) {
-            ++left;
-        }
-        while(right - 1 >= 0 && arr[right] >= arr[right-1]) {
+        int right = n-1, left = 0;
+        while(right > 0 && arr[right] >= arr[right-1]) {
             --right;
         }
-        if(left >= right) {
-            return 0;
-        }
-        // either remove amount after left OR everything left of the right
-        int ans = min(n - (left + 1), right);
-        for (int i = 0; i <= left; i++) {
-            int target = arr[i];
-            int j = helperBinarySearch(arr, right, n, target);
-            ans = min(ans, j - (i + 1));
+        int ans = right;
+        while(left < right && (left == 0 || arr[left-1] <= arr[left])) {
+            while(right < arr.size() && arr[left] > arr[right]) {
+                ++right;
+            }
+            ans = min(ans, right - left - 1);
+            ++left;
         }
         return ans;
     }
