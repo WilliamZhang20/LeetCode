3d97b8eafc0b0b733ab5800ef8f71097d7805e57
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2026-01-13 16:40:04 -0500

    Runtime: 28 ms (42.18%), Memory: 27.5 MB (28.66%) - Gitcode

--- 1143-longest-common-subsequence/1143. Longest Common Subsequence.cpp
+++ 1143-longest-common-subsequence/1143. Longest Common Subsequence.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.length();
-        int m = text2.length();
-        if(n < m) {
-            swap(text1, text2);
-            swap(n, m);
-        }
-        vector<int> row1(m+1, 0);
-        vector<int> row2(m+1, 0);
-        for(int i=1; i<n+1; i++) {
-            for(int j=1; j<m+1; j++) {
+        int n = text1.length(), m = text2.length();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=m; j++) {
                 if(text1[i-1] == text2[j-1]) {
-                    row1[j] = row2[j-1] + 1;
-                } else {
-                    row1[j] = max(row2[j], row1[j-1]);
+                    dp[i][j] = dp[i-1][j-1] + 1; // continue subsequence
+                }
+                else {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                 }
             }
-            row2 = row1;
-            row1.assign(m+1, 0);
         }
-        return row2[m];
+        return dp[n][m];
     }
 };
