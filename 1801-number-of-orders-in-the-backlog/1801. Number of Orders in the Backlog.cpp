5292c229fcdd66a332cec20b359773dc21096f91
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2026-01-23 10:49:10 -0500

    Runtime: 156 ms (72.70%), Memory: 62.8 MB (67.06%) - Gitcode

--- 1801-number-of-orders-in-the-backlog/1801. Number of Orders in the Backlog.cpp
+++ 1801-number-of-orders-in-the-backlog/1801. Number of Orders in the Backlog.cpp
@@ -1,46 +1,38 @@
 class Solution {
 public:
-    static constexpr int MOD = 1e9+7;
-    using Order = array<int,2>;
+    static constexpr int MOD = 1e9 + 7;
     int getNumberOfBacklogOrders(vector<vector<int>>& orders) {
-        // buy want smallest sell, sell wants largest buy
-        priority_queue<Order, vector<Order>, greater<>> asks;
-        priority_queue<Order> bids;
-        
-        for(auto& order: orders) {
-            int p = order[0], a = order[1], t = order[2];
-            if(t == 0) { // incoming buy order (bid)
-                // if match with cheapest sell, clear
-                while(a > 0 && !asks.empty() && asks.top()[0] <= p) {
-                    auto cur = asks.top();
-                    asks.pop();
-                    int matched = min(a, cur[1]);
-                    // decrement amount
-                    // decrement sellAmount
-                    a -= matched;
-                    cur[1] -= matched;
-                    
-                    // if sellAmount > 0, push back into asks
-                    if(cur[1]) asks.push(cur);
+        using A = array<int,2>; // {price, amount}
+        priority_queue<A, vector<A>, greater<>> ask;
+        priority_queue<A> bid;
+
+        for (auto &o : orders) {
+            int p = o[0], a = o[1], t = o[2];
+
+            if (t == 0) { // buy
+                while (a && !ask.empty() && ask.top()[0] <= p) {
+                    auto cur = ask.top(); ask.pop();
+                    int m = min(a, cur[1]);
+                    a -= m;
+                    cur[1] -= m;
+                    if (cur[1]) ask.push(cur);
                 }
-                // else if amount > 0, push into bids
-                if (a) bids.push({p, a});
-            } else { // incoming sell order (ask)
-                // if match with most expensive buy, clear
-                while(a && !bids.empty() && bids.top()[0] >= p) {
-                    auto cur = bids.top();
-                    bids.pop();
-                    int matched = min(a, cur[1]);
-                    a -= matched;
-                    cur[1] -= matched;
-                    if(cur[1]) bids.push(cur);
+                if (a) bid.push({p, a});
+            } else { // sell
+                while (a && !bid.empty() && bid.top()[0] >= p) {
+                    auto cur = bid.top(); bid.pop();
+                    int m = min(a, cur[1]);
+                    a -= m;
+                    cur[1] -= m;
+                    if (cur[1]) bid.push(cur);
                 }
-                if(a) asks.push({p, a});
+                if (a) ask.push({p, a});
             }
         }
+
         long long res = 0;
-        while (!bids.empty()) res = (res + bids.top()[1]) % MOD, bids.pop();
-        while (!asks.empty()) res = (res + asks.top()[1]) % MOD, asks.pop();
+        while (!bid.empty()) res = (res + bid.top()[1]) % MOD, bid.pop();
+        while (!ask.empty()) res = (res + ask.top()[1]) % MOD, ask.pop();
         return res;
     }
 };
