4966d467eea7a89f3cb3c137ced0301bb9831828
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2026-01-23 20:54:52 -0500

    Runtime: 0 ms (100.00%), Memory: 97.5 MB (24.41%) - Gitcode

--- 121-best-time-to-buy-and-sell-stock/121. Best Time to Buy and Sell Stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121. Best Time to Buy and Sell Stock.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int buy = prices[0];
-        int profit = 0;
-        for(int i=1; i<prices.size(); i++) {
-            buy = min(buy, prices[i]);
-            profit = max(profit, prices[i] - buy);
+        int cur = INT_MAX, ans = 0;
+        for(int i=0; i < prices.size(); i++) {
+            cur = min(prices[i], cur);
+            ans = max(prices[i] - cur, ans);
         }
-        return profit;
+        return ans;
     }
 };
