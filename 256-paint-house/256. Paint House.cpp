4996e48271a5aca3224daa779f0ca18e76ed86bb
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2026-01-26 22:02:02 -0500

    Runtime: 0 ms (100.00%), Memory: 12.9 MB (100.00%) - Gitcode

---
+++ 256-paint-house/256. Paint House.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minCost(vector<vector<int>>& costs) {
+        if(costs.size() == 0) return 0;
+        for(int n = costs.size() - 2; n >= 0; n--) {
+            costs[n][0] += min(costs[n+1][1], costs[n+1][2]);
+            costs[n][1] += min(costs[n+1][0], costs[n+1][2]);
+            costs[n][2] += min(costs[n+1][0], costs[n+1][1]);
+        }
+        return min(min(costs[0][0], costs[0][1]), costs[0][2]);
+    }
+};
