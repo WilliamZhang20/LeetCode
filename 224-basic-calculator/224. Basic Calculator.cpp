92311a8351a2b2e55f593c95962123f98bfd9a5a
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2026-01-31 18:22:55 -0500

    Runtime: 0 ms (100.00%), Memory: 12.6 MB (75.24%) - Gitcode

--- 224-basic-calculator/224. Basic Calculator.cpp
+++ 224-basic-calculator/224. Basic Calculator.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
-    bool isDigit(char c) {
-        return c >= '0' && c <= '9';
-    }
     int calculate(string s) {
         long long n = 0;
-        int sign = 1;
         long long result = 0;
-        stack<long long> st;
-        for(char& c: s) {
-            if(isDigit(c)) {
+        int sign = 1;
+
+        vector<long long> st;
+        st.reserve(32); // avoids reallocs
+
+        for (char c : s) {
+            if (c >= '0' && c <= '9') {
                 n = n * 10 + (c - '0');
-            } else if(c == '+' or c == '-') {
+            }
+            else if (c == '+' || c == '-') {
                 result += sign * n;
                 n = 0;
                 sign = (c == '+') ? 1 : -1;
-            } else if(c == '(') {
-                st.push(result);
-                st.push(sign);
+            }
+            else if (c == '(') {
+                st.push_back(result);
+                st.push_back(sign);
                 result = 0;
                 sign = 1;
-            } else if(c == ')') {
+            }
+            else if (c == ')') {
                 result += sign * n;
                 n = 0;
-                result *= st.top();
-                st.pop();
-                result += st.top();
-                st.pop();
+
+                result *= st.back(); // sign
+                st.pop_back();
+
+                result += st.back(); // previous result
+                st.pop_back();
             }
         }
+
         return (int)(result + sign * n);
     }
 };
