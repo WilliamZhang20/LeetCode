8be9598088b2a397e5e9566e8a494ab10a1081a3
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2026-03-04 19:51:12 -0500

    Runtime: 11 ms (15.65%), Memory: 26.5 MB (82.31%) - Gitcode

--- 1333-filter-restaurants-by-vegan-friendly-price-and-distance/1333. Filter Restaurants by Vegan-Friendly, Price and Distance.py
+++ 1333-filter-restaurants-by-vegan-friendly-price-and-distance/1333. Filter Restaurants by Vegan-Friendly, Price and Distance.py
@@ -1,3 +1,3 @@
 class Solution:
  def filterRestaurants(s,r,v,p,d):
-  return[i for i,s,f,m,k in sorted(r,key=lambda x:(-x[1],-x[0]))if f>=v and m<=p and k<=d]
+  return[i for i,a,b,c,e in sorted(r,key=lambda x:(-x[1],-x[0]))if b>=v and c<=p and e<=d]
