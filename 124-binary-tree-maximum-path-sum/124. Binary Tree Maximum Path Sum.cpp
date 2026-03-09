76a072710287f27d8d744f35bf3d0d1fe3fd1b01
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2026-03-08 21:23:36 -0400

    Runtime: 1 ms (18.82%), Memory: 28 MB (46.24%) - Gitcode

--- 124-binary-tree-maximum-path-sum/124. Binary Tree Maximum Path Sum.cpp
+++ 124-binary-tree-maximum-path-sum/124. Binary Tree Maximum Path Sum.cpp
@@ -10,21 +10,18 @@
  * };
  */
 class Solution {
-    int maxSum;
+    int max_sum;
 public:
-    int helper(TreeNode* root) {
-        if(!root) {
-            return 0;
-        }
-        int left = max(0, helper(root->left));
-        int right = max(0, helper(root->right));
-
-        maxSum = max(maxSum, left + right + root->val);
-        return root->val + max(left, right);
+    int gain_from_subtree(TreeNode* root) {
+        if(!root) return 0;
+        int gain_from_left = max(gain_from_subtree(root->left), 0);
+        int gain_from_right = max(gain_from_subtree(root->right), 0);
+        max_sum = max(root->val + gain_from_left + gain_from_right, max_sum);
+        return root->val + max(gain_from_left, gain_from_right);
     }
     int maxPathSum(TreeNode* root) {
-        maxSum=root->val;
-        helper(root);
-        return maxSum;
+        max_sum = INT_MIN;
+        gain_from_subtree(root);
+        return max_sum;
     }
 };
