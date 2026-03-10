6f0065f298174495d02cacbf8f1c659ba604d2c9
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2026-03-10 19:45:17 -0400

    Runtime: 0 ms (100.00%), Memory: 16.1 MB (91.19%) - Gitcode

--- 909-snakes-and-ladders/909. Snakes and Ladders.cpp
+++ 909-snakes-and-ladders/909. Snakes and Ladders.cpp
@@ -3,33 +3,33 @@ public:
     int snakesAndLadders(vector<vector<int>>& board) {
         int n = board.size();
         int n2 = n * n;
-        vector<int> dist(n2+1, 1000);
         queue<int> q;
         vector<bool> vis(n2+1, false);
         q.push(1);
-        dist[1] = 0;
+        vis[1] = true;
+        int steps = 0;
         while(!q.empty()) {
-            // convert from cell value to i, j: [n, x / n]
-            int val = q.front();
-            q.pop();
-            if(vis[val]) continue;
-            vis[val] = true;
-            for(int x=1; x<=6; x++) {
-                if(val + x > n2) break;
-                int r = (val + x - 1) / n; 
-                int c = (val + x - 1) % n;
-                if (r % 2 == 1) c = n - 1 - c;
-                r = n - 1 - r;
-                if(board[r][c] != -1) {
-                    q.push(board[r][c]);
-                    dist[board[r][c]] = min(dist[board[r][c]], dist[val] + 1);
-                } else if(!vis[val + x]) {
-                    dist[val + x] = min(dist[val + x], dist[val] + 1);
-                    q.push(val + x);
+            int sz = q.size();
+            while(sz--) {
+                int cur = q.front();
+                q.pop();
+                if(cur == n2) return steps;
+                for(int d=1; d<=6 && cur + d <= n2; d++) {
+                    int nxt = cur + d;
+                    int r = (nxt - 1) / n;
+                    int c = (nxt - 1) % n;
+                    if(r % 2) c = n - 1 - c;
+                    r = n - 1 - r;
+                    if(board[r][c] != -1)
+                        nxt = board[r][c];
+                    if(!vis[nxt]) {
+                        vis[nxt] = true;
+                        q.push(nxt);
+                    }
                 }
             }
+            steps++;
         }
-        if(dist[n2] == 1000) return -1;
-        return dist[n2];
+        return -1;
     }
 };
