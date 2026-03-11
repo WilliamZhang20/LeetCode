8651d05b0607c29874c48611452f456111bfc726
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2026-03-11 10:19:41 -0400

    Runtime: 0 ms (100.00%), Memory: 11.4 MB (63.73%) - Gitcode

--- 76-minimum-window-substring/76. Minimum Window Substring.cpp
+++ 76-minimum-window-substring/76. Minimum Window Substring.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int l=0;
-        unordered_map<char, int> tgt;
-        for(char& c: t) {
-            tgt[c]++;
-        }
-        unordered_map<char, int> curTrack;
-        int minLength = 1e6;
-        string ans;
-        for(int r=0; r<s.length(); r++) {
-            curTrack[s[r]]++; // add to frequency
-            bool d = true;
-            for(auto& [k, v]: tgt) {
-                if(v > curTrack[k]) {
-                    d = false;
-                    break;
+        vector<int> need(128, 0);
+        for(char c : t) need[c]++;
+        int required = t.size();
+        int l = 0;
+        int minLen = INT_MAX;
+        int start = 0;
+        for(int r = 0; r < s.size(); r++) {
+            if(need[s[r]] > 0) required--;
+            need[s[r]]--;
+            while(required == 0) {
+                if(r - l + 1 < minLen) {
+                    minLen = r - l + 1;
+                    start = l;
                 }
-            }
-            if(!d) continue; // keep increasing r until valid
-            while(curTrack[s[l]] > tgt[s[l]]) { // keep shrinking while valid
-                curTrack[s[l]]--;
+                need[s[l]]++;
+                if(need[s[l]] > 0) required++;
                 l++;
             }
-            if(r - l + 1 < minLength) {
-                minLength = r - l + 1;
-                ans = s.substr(l, r - l + 1);
-            } 
         }
-        return ans;
+        return minLen == INT_MAX ? "" : s.substr(start, minLen);
     }
 };
