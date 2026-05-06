c74ab15b8c444c39abb1a84e9bc433d4e30a2faa
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2026-05-06 18:28:14 -0400

    Runtime: 19 ms (8.35%), Memory: 22.9 MB (76.83%) - Gitcode

--- 875-koko-eating-bananas/875. Koko Eating Bananas.cpp
+++ 875-koko-eating-bananas/875. Koko Eating Bananas.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
         sort(piles.begin(), piles.end());
-        int l = 1, r = piles[piles.size()-1];
-        int ans = INT_MAX;
+        // function of time to eat of speed = # of piles less than speed + quotients rounded up of remaining
+        int r = piles[piles.size()-1];
+        int l = 1;
         while(l < r) {
             int k = (l + r) / 2;
             int time = 0;
-            for(int i=0; i<piles.size(); i++) {
-                time += (piles[i] + k - 1) / k;
+            for(int i=0; i < piles.size(); i++) {
+                time += ceil(piles[i] / (k * 1.0));
             }
-            cout << time << endl;
             if(time > h) {
                 l = k + 1;
-            } else {
+            }
+            else if(time <= h) {
                 r = k;
             }
         }
-        return r;
+        return l;
     }
 };
