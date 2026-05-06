3c6e7d0ad593cf68b06f391c08b5b87859c66a44
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2026-05-06 18:50:26 -0400

    Runtime: 79 ms (6.44%), Memory: 31 MB (38.97%) - Gitcode

--- 221-maximal-square/221. Maximal Square.cpp
+++ 221-maximal-square/221. Maximal Square.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
-        int maxLen = 0;
-        for(int i=1; i<=m; i++) {
-            for(int j=1; j<=n; j++) {
-                if(matrix[i-1][j-1] == '1') {
-                    dp[i][j] = min(dp[i-1][j], min(dp[i][j-1], dp[i-1][j-1])) + 1;
-                    maxLen = max(maxLen, dp[i][j]);
+        int n = matrix.size(), m = matrix[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        int ans = 0;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(matrix[i][j] == '1') {
+                    if(i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+                    }
+                    
+                    ans = max(ans, dp[i][j]);
                 }
             }
         }
-        return maxLen * maxLen;
+        return ans * ans;
     }
 };
