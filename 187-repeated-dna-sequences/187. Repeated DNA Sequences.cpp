19420ed77b4e497cf857f22bcb0bb452df978eae
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2026-06-06 13:17:47 -0400

    Runtime: 23 ms (99.21%), Memory: 18.4 MB (96.49%) - Gitcode

--- 187-repeated-dna-sequences/187. Repeated DNA Sequences.cpp
+++ 187-repeated-dna-sequences/187. Repeated DNA Sequences.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
-        unordered_set<string> seen;
-        unordered_set<string> res;
-        for(int start=0; start + 10 <= s.length(); ++start) {
-            string tmp = s.substr(start, 10);
-            if(seen.count(tmp)) res.insert(tmp);
-            seen.insert(tmp);
+        if (s.size() < 10) return {};
+        vector<string> ans;
+        unordered_map<int, int> seqFreq;
+        int hash = 0;
+        unordered_map<char, int> encode{
+            {'A',0},
+            {'C',1},
+            {'G',2},
+            {'T',3}
+        };
+        for (int i = 0; i < 10; i++) {
+            hash = (hash << 2) | encode[s[i]];
         }
-        return vector<string>(res.begin(), res.end());
+        seqFreq[hash]++;
+        for(int i=10; i<s.length(); i++) {
+            hash = ((hash << 2) | encode[s[i]]) & ((1 << 20) - 1);
+            seqFreq[hash]++;
+            if(seqFreq[hash] == 2) {
+                ans.emplace_back(s.substr(i-9, 10));
+            }
+        }
+        return ans;
     }
 };
