8cc9450e0788da07aa45bf68629c40e4c5818b7d
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2026-06-06 13:31:06 -0400

    Runtime: 0 ms (100.00%), Memory: 14.9 MB (81.37%) - Gitcode

--- 2574-left-and-right-sum-differences/2574. Left and Right Sum Differences.cpp
+++ 2574-left-and-right-sum-differences/2574. Left and Right Sum Differences.cpp
@@ -3,13 +3,13 @@ public:
     vector<int> leftRightDifference(vector<int>& nums) {
         int n = nums.size();
         vector<int> ans(n, 0);
-        int prefix = 0;
-        for(int i=0; i<n; i++) {
+        int prefix = nums[0];
+        for(int i=1; i<n; i++) {
             ans[i] = prefix;
             prefix += nums[i];
         }
-        int suffix = 0;
-        for(int i=n-1; i>=0; i--) {
+        int suffix = nums[n-1];
+        for(int i=n-2; i>=0; i--) {
             ans[i] = abs(ans[i]-suffix);
             suffix += nums[i];
         }
